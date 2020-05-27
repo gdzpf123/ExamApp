693d159e5425cff2f283d4b006688cc25acd6866
Administrator <asdas>
2020-05-27 09:45:14 +0800

    asdasd

---
+++ app/src/main/java/com/example/examapp/Entity/RadioEntity.java
@@ -0,0 +1,67 @@
+package com.example.examapp.Entity;
+
+public class RadioEntity {
+    private int id;
+    private String title;
+    private String ansA;
+    private String ansB;
+    private String ansC;
+    private String ansCD;
+    private String ans;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getAnsA() {
+        return ansA;
+    }
+
+    public void setAnsA(String ansA) {
+        this.ansA = ansA;
+    }
+
+    public String getAnsB() {
+        return ansB;
+    }
+
+    public void setAnsB(String ansB) {
+        this.ansB = ansB;
+    }
+
+    public String getAnsC() {
+        return ansC;
+    }
+
+    public void setAnsC(String ansC) {
+        this.ansC = ansC;
+    }
+
+    public String getAnsCD() {
+        return ansCD;
+    }
+
+    public void setAnsCD(String ansCD) {
+        this.ansCD = ansCD;
+    }
+
+    public String getAns() {
+        return ans;
+    }
+
+    public void setAns(String ans) {
+        this.ans = ans;
+    }
+}
